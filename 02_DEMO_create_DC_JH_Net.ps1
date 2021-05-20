1ccb21e6535f2cc04c4f95b9f203a2c5ab9dd4cd
nimburain <manuel.fritz@gmail.com>
2021-05-20 15:44:10 +0200

    changed URL

--- 02_DEMO_create_DC_JH_Net.ps1
+++ 02_DEMO_create_DC_JH_Net.ps1
@@ -16,10 +16,10 @@ $templateParameterObject1 = @{
 $deploymentstart = Get-Date
 
 #Deploy the network
-New-AzResourceGroupDeployment -ResourceGroupName 'rg-demo-basics' -Name 'NetworkSetup' -Mode Incremental -TemplateUri 'https://raw.githubusercontent.com/nimburain/scripts/main/01-ARM_Network.json'
+New-AzResourceGroupDeployment -ResourceGroupName 'rg-demo-basics' -Name 'NetworkSetup' -Mode Incremental -TemplateUri 'https://raw.githubusercontent.com/nimburain/HorizonOnAzureDemo/master/BaseSetupArtefacts/01-ARM_Network.json'
 
 #Deploy the VM and make it a domain controller
-New-AzResourceGroupDeployment -ResourceGroupName 'rg-demo-basics' -Name 'DCSetup' -Mode Incremental -TemplateUri 'https://raw.githubusercontent.com/nimburain/scripts/main/02-ARM_AD.json' -TemplateParameterObject $templateParameterObject1
+New-AzResourceGroupDeployment -ResourceGroupName 'rg-demo-basics' -Name 'DCSetup' -Mode Incremental -TemplateUri 'https://raw.githubusercontent.com/nimburain/HorizonOnAzureDemo/master/BaseSetupArtefacts/02-ARM_AD.json' -TemplateParameterObject $templateParameterObject1
 
 #make sure DC is new DNS server in this VNET  
 az network vnet update -g 'rg-demo-basics' -n 'SDBOX-VNET' --dns-servers 10.0.0.4 
@@ -57,19 +57,19 @@ $templateParameterObject2 = @{
 'DiskSku' = [string] 'StandardSSD_LRS'
 'DomainName' = [string] 'demo.local'
 }
-New-AzResourceGroupDeployment -ResourceGroupName 'rg-demo-basics' -Name 'FileServerSetup' -Mode Incremental -TemplateUri 'https://raw.githubusercontent.com/nimburain/scripts/main/03-ARM_FS.json' -TemplateParameterObject $templateParameterObject2
+New-AzResourceGroupDeployment -ResourceGroupName 'rg-demo-basics' -Name 'FileServerSetup' -Mode Incremental -TemplateUri 'https://raw.githubusercontent.com/nimburain/HorizonOnAzureDemo/master/BaseSetupArtefacts/03-ARM_FS.json' -TemplateParameterObject $templateParameterObject2
 
 #cleanup: remove 'DCInstall' extension
 Remove-AzVMCustomScriptExtension -Name 'DCInstall' -VMName $($templateParameterObject1.vmName) -ResourceGroupName 'rg-demo-basics' -Force  
 
 #Do post AD installation steps: e.g. create OUs and some Horizon View Demo Users.
-Set-AzVMCustomScriptExtension -Name 'PostDCActions' -VMName $($templateParameterObject1.vmName) -ResourceGroupName 'rg-demo-basics' -Location (Get-AzVM -ResourceGroupName 'rg-demo-basics' -Name $($templateParameterObject1.vmName)).Location -Run 'CSE_AD_Post.ps1' -Argument "HorizonView $($credential.GetNetworkCredential().Password)" -FileUri 'https://raw.githubusercontent.com/nimburain/scripts/main/CSE_AD_Post.ps1'  
+Set-AzVMCustomScriptExtension -Name 'PostDCActions' -VMName $($templateParameterObject1.vmName) -ResourceGroupName 'rg-demo-basics' -Location (Get-AzVM -ResourceGroupName 'rg-demo-basics' -Name $($templateParameterObject1.vmName)).Location -Run 'CSE_AD_Post.ps1' -Argument "HorizonView $($credential.GetNetworkCredential().Password)" -FileUri 'https://raw.githubusercontent.com/nimburain/HorizonOnAzureDemo/master/BaseSetupArtefacts/CSE_AD_Post.ps1'  
   
 #Cleanup
 Remove-AzVMCustomScriptExtension -Name 'PostDCActions' -VMName $($templateParameterObject1.vmName) -ResourceGroupName 'rg-demo-basics' -Force -NoWait
 
 # make this server a file server.
-Set-AzVMCustomScriptExtension -Name 'FileServerInstall' -VMName $($templateParameterObject2.vmName) -ResourceGroupName 'rg-demo-basics' -Location (Get-AzVM -ResourceGroupName 'rg-demo-basics' -Name $($templateParameterObject2.vmName)).Location -Run 'CSE_FS.ps1' -FileUri 'https://raw.githubusercontent.com/nimburain/scripts/main/CSE_FS.ps1' 
+Set-AzVMCustomScriptExtension -Name 'FileServerInstall' -VMName $($templateParameterObject2.vmName) -ResourceGroupName 'rg-demo-basics' -Location (Get-AzVM -ResourceGroupName 'rg-demo-basics' -Name $($templateParameterObject2.vmName)).Location -Run 'CSE_FS.ps1' -FileUri 'https://raw.githubusercontent.com/nimburain/HorizonOnAzureDemo/master/BaseSetupArtefacts/CSE_FS.ps1' 
 
 #Cleanup
 Remove-AzVMCustomScriptExtension -Name 'FileServerInstall' -VMName $($templateParameterObject2.vmName) -ResourceGroupName 'rg-demo-basics' -Force -NoWait  
