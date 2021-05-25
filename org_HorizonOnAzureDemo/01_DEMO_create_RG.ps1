99aa8be875cfe3b04c26e7a84f448a2a2ed5586f
nimburain <82578313+nimburain@users.noreply.github.com>
2021-05-25 09:43:35 +0200

    Add files via upload

---
+++ org_HorizonOnAzureDemo/01_DEMO_create_RG.ps1
@@ -0,0 +1,26 @@
+$RGPrefix = "rg-demo-"
+$RGSuffixes = @("basics","pool01","pool02","pool03")
+$RGLocation = ""
+
+#select your deployment region
+do {
+    $regions = @("")
+    Get-AzLocation | foreach -Begin { $i = 0 } -Process {
+        $i++
+        $regions += "{0}. {1}" -f $i, $_.Location
+    } -outvariable menu
+    $regions | Format-Wide { $_ } -Column 4 -Force
+    $r = Read-Host "Select a region to deploy to by number"
+    $RGLocation = $regions[$r].Split()[1]
+    if ($RGLocation -eq $null) { Write-Host "You must make a valid selection" -ForegroundColor Red }
+    else {
+        Write-Host "Selecting region $($regions[$r])" -ForegroundColor Green
+    }
+}
+until ($RGLocation -ne $null)
+
+foreach ($RGSuffix in $RGSuffixes)
+{
+   New-AzResourceGroup -Name "$($RGPrefix)$($RGSuffix)" -Location $RGLocation
+}  
+#remember your region :-) 
